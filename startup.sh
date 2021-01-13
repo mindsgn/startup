a73c5e6a150e7f8c94f997cc5aecb81ffe2ea39d
mindsgn <seni.tembe@protonmail.com>
2021-01-13 22:00:54 +0200

    update

---
+++ startup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+source ~/.bash_profile 
+source ~/.zshrc
+source ~/.profile
+source ~/.bashrc
